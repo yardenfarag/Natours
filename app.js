2ce7da805688387e90cb1bdf6d5ebf05e897580b
Yarden Farag <yarden.farag@gmail.com>
2023-04-09 17:00:06 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const express = require('express')
+
+const app = express()
+
+// app.get('/', (req, res) => {
+//     res.status(200).json({message: 'Hello from the server side!', app: 'Natours'})
+// })
+
+// app.post('/', (req, res) => {
+//     res.send('You can post here')
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+})
+
+const port = 4000
+app.listen(port, () => {
+    console.log(`App listening on ${port}`)
+})
